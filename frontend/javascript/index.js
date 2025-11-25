89dea00fa7ccc62e2e1faec2a4463c821d972472
Beni-256 <bugattibeni@gmail.com>
2025-11-25 16:30:51 +0100

    Oldal Felépítése
    Oldal Felépítése:
    Div-ek, gombok, ablakok kezdetleges veziója.

---
+++ frontend/javascript/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    document.getElementById("home").addEventListener("click", () =>{
+
+    });
+    document.getElementById("personal").addEventListener("click", () =>{
+
+    });
+    document.getElementById("map").addEventListener("click", () =>{
+
+    });
+    document.getElementById("profile").addEventListener("click", () =>{
+
+    });
+
+    document.getElementById("login").addEventListener("click", () =>{
+
+    });
+    document.getElementById("create").addEventListener("click", () =>{
+
+    });
+    document.getElementById("settings").addEventListener("click", () =>{
+
+    });
+
+    document.getElementById("today").addEventListener("click", () =>{
+
+    });
+    document.getElementById("week").addEventListener("click", () =>{
+
+    });
+    document.getElementById("month").addEventListener("click", () =>{
+
+    });
+    document.getElementById("top").addEventListener("click", () =>{
+
+    });
+    document.getElementById("new").addEventListener("click", () =>{
+
+    });
+    document.getElementById("near").addEventListener("click", () =>{
+
+    });
+})
