1e4d51d929308855809f71f12419d5a2d83c60bb
Zone1411-HK <kevinke2005@gmail.com>
2025-12-16 15:02:21 +0100

    Regisztráció toggle
    A login mintájára egy alap regisztráció form. Valamint lehet toggle-ni a bejelentkezést és regisztrációt

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('profile').addEventListener('click', () => {});
 
     document.getElementById('loginButton').addEventListener('click', login);
+    document.getElementById('regText').addEventListener('click', toggleLoginRegistration);
+    document.getElementById('loginText').addEventListener('click', toggleLoginRegistration);
     document.getElementById('create').addEventListener('click', () => {});
     document.getElementById('settings').addEventListener('click', () => {});
 
@@ -15,3 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('new').addEventListener('click', () => {});
     document.getElementById('near').addEventListener('click', () => {});
 });
+
+//! Ha tudtok pls adjatok neki jobb nevet 🙏
+function toggleLoginRegistration() {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.classList.toggle('invisible');
+
+    const regForm = document.getElementById('regForm');
+    regForm.classList.toggle('invisible');
+}
