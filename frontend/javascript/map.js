e502ca577c7aad1aa34671a8743116af0e3475ae
Zone1411-HK <kevinke2005@gmail.com>
2026-02-24 16:04:24 +0100

    mapozgatás

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -1,18 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let map = L.map('map').setView([51.505, -0.09], 5);
+    let map = L.map('map', {
+        zoomControl: false,
+        maxBounds: [
+            [-90, -180],
+            [90, 180]
+        ],
+        maxBoundsViscosity: 1
+    }).setView([51.505, -0.09], 5);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
+        minZoom: 2.5,
+        noWrap: true,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    let markers = L.markerClusterGroup({ maxClusterRadius: 150 });
-    for (let i = 0; i < 5000; i++) {
-        let x = Math.floor(Math.random() * 41);
-        let y = Math.floor(Math.random() * 101);
+
+    let markers = L.markerClusterGroup({ maxClusterRadius: 1 });
+    for (let i = 0; i < 100; i++) {
+        let x = Math.floor(Math.random() * 360 - 180);
+        let y = Math.floor(Math.random() * 180 - 90);
         placeMarker(x, y, map, '/images/placeholder2.gif', markers);
+        map.addLayer(
+            new L.Marker([y, x], {
+                icon: new L.icon({
+                    iconUrl: '/images/map-pin.svg',
+                    iconSize: [40, 40],
+                    iconAnchor: [20, 20]
+                })
+            })
+        );
     }
     //placeMarker(-0.09, 51.505, map, '/images/placeholder2.gif', markers);
 
-    map.addLayer(markers);
+    //map.addLayer(markers);
+    document.getElementById('goBack').addEventListener('click', () => {
+        window.location.href = '/';
+    });
 });
 
 function placeMarker(x, y, map, link, markers) {
